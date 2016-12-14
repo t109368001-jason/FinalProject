d4be5877171b221d289ad96142de9f4bda82489a
Xiao-Xian-Jie <t105360046@ntut.org.tw>
2016-12-14 21:30:39 +0800

    XIAO

---
+++ include/xiao.h
@@ -0,0 +1,10 @@
+void delay_ms(int x)
+{
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < 1000; j++)
+		{
+			for (int k = 0; k < 518; k++);
+		}
+	}
+}
--- source/main.c
+++ source/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <conio.h>
 #include <stdlib.h>
-#include "kai.h"
+#include "xiao.h"
 
 int main(void)
 {
+	int question[9][9];
+	int do_question[9][9];
+	int cursor[2] = { 0, 0 };
+	int level = 0;
+	int question_number = 0;
+	char c;
+	c = _getche();
+	//printf_menu();
+
+	//set_question(question, level, question_number);
+	
+	//printf_question(question, cursor);
 
 	system("pause");
 	return 0;
