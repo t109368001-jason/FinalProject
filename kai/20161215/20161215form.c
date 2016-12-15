3d89fb43aff1bf04fd87e9ba7b44d9f225d0a392
t105360045 <t105360045@ntut.org.tw>
2016-12-15 15:10:40 +0800

    kai

--- kai/20161215/20161215form.c
+++ kai/20161215/20161215form.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void form(void);
+
 int main(void)
 {
 	
@@ -11,4 +14,47 @@ int main(void)
 	system("pause");
 	return 0;
 	
+
+
+
+
+}
+
+
+
+
+void form(void)
+{
+	int init_num_f = 1;
+	int cnt_1 = 0;
+	if (init_num_f)
+	{
+		for (int i = 0; i <= 18; i++)
+		{
+			for (int j = 0; j <= 18; j++)
+			{
+				f[i][j] = cnt_1;
+				cnt_1++;
+			}
+		}
+		init_num_f = 0;
+	}
+	for (int i = 0; i <= 18; i++)
+	{
+		for (int j = 0; j <= 18; j++)
+		{
+			switch(i)
+			{
+				case 0:
+					
+					break;
+					
+				case 1:
+					
+					break;
+					
+			}
+
+		}
+	}
 }
