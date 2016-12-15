cfa771f9edc814434db26d8ca4a8d78d1ec35eb1
t105360045 <t105360045@ntut.org.tw>
2016-12-15 15:01:20 +0800

    kai

--- kai/20161215/project/source/Main.c
+++ kai/20161215/project/source/Main.c
@@ -1,16 +1,101 @@
-/*
-這是編序號的程式
-20161213
-first
-計算空格需要19*19
-每單位
-*/
+/*20161215*/
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <windows.h>
+////////////////////////////////////
 int f[19][19] = { 0 };
 
+
+/////////////////////////////////////
+void ruler_x(void);
+void ruler_y(void);
+void ruler(void);
+void printf_decision(void);
+void gotoxy(int xpos, int ypos);
+void form(void);
+////////////////////////////////////
 int main(void)
+{
+
+
+
+
+	form();
+	ruler();
+	//printf_decision();
+
+
+
+
+	system("pause");
+	return 0;
+}
+
+void gotoxy(int xpos, int ypos)
+{
+	COORD scrn;
+	HANDLE hOuput = GetStdHandle(STD_OUTPUT_HANDLE);
+	scrn.X = xpos; scrn.Y = ypos;
+	SetConsoleCursorPosition(hOuput, scrn);
+}
+
+void ruler_x(void)
+{
+
+	for (int i = 0; i <= 109; i++)
+		printf("%d", i % 10);
+
+}
+
+void ruler_y(void)
+{
+
+	for (int i = 1; i <= 27; i++)
+	{
+		gotoxy(0, i);
+		printf("%d", i);
+		
+	}
+
+}
+
+void ruler(void)
+{
+	ruler_x();
+	printf("\n");
+	ruler_y();
+}
+
+void printf_decision(void)
+{
+	int a;
+
+
+	gotoxy(24, 12);
+	printf("請選擇選項");
+	gotoxy(24, 13);
+	printf("1:確認\n");
+	gotoxy(24, 14);
+	printf("2:取消\n");
+	scanf_s("%d", &a);
+	fflush(stdin);
+
+	switch (a)
+	{
+	case 1:
+		gotoxy(24, 12);
+		printf("1:確認");
+		break;
+	case 2:
+		gotoxy(24, 12);
+		printf("2:取消");
+		break;
+	default:
+		break;
+	}
+}
+
+void form(void)
 {
 	int init_num_f = 1;
 	int cnt_1 = 0;
@@ -34,9 +119,5 @@ int main(void)
 
 		}
 	}
-
-	system("pause");
-	return 0;
 }
-
 
