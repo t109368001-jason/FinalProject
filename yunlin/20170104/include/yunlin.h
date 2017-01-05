f746c40afe750504c3e9f6f2169c17ca05d4c33a
yunlin <t105360007@ntut.org.tw>
2017-01-05 14:07:26 +0800

    yunlin

---
+++ yunlin/20170104/include/yunlin.h
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+char *yunlin_road_question[5][20] = 
+{
+	{ "../../../data/01question/0101.txt", "../../../data/01question/0102.txt", "../../../data/01question/0103.txt", "../../../data/01question/0104.txt", "../../../data/01question/0105.txt", "../../../data/01question/0106.txt", "../../../data/01question/0107.txt", "../../../data/01question/0108.txt", "../../../data/01question/0109.txt", "../../../data/01question/0110.txt" },
+	{ "../../../data/02question/0101.txt", "../../../data/02question/0102.txt", "../../../data/02question/0103.txt", "../../../data/02question/0104.txt", "../../../data/02question/0105.txt", "../../../data/02question/0106.txt", "../../../data/02question/0107.txt", "../../../data/02question/0108.txt", "../../../data/02question/0109.txt", "../../../data/02question/0110.txt" },
+	{ "../../../data/03question/0101.txt", "../../../data/03question/0102.txt", "../../../data/03question/0103.txt", "../../../data/03question/0104.txt", "../../../data/03question/0105.txt", "../../../data/03question/0106.txt", "../../../data/03question/0107.txt", "../../../data/03question/0108.txt", "../../../data/03question/0109.txt", "../../../data/03question/0110.txt" },
+	{ "../../../data/04question/0101.txt", "../../../data/04question/0102.txt", "../../../data/04question/0103.txt", "../../../data/04question/0104.txt", "../../../data/04question/0105.txt", "../../../data/04question/0106.txt", "../../../data/04question/0107.txt", "../../../data/04question/0108.txt", "../../../data/04question/0109.txt", "../../../data/04question/0110.txt" },
+	{ "../../../data/05question/0101.txt", "../../../data/05question/0102.txt", "../../../data/05question/0103.txt", "../../../data/05question/0104.txt", "../../../data/05question/0105.txt", "../../../data/05question/0106.txt", "../../../data/05question/0107.txt", "../../../data/05question/0108.txt", "../../../data/05question/0109.txt", "../../../data/05question/0110.txt" }
+};
+
+char *yunlin_road_answer[5][20] =
+{
+	{ "../../../data/01answer/0101.txt", "../../../data/01answer/0102.txt", "../../../data/01answer/0103.txt", "../../../data/01answer/0104.txt", "../../../data/01answer/0105.txt", "../../../data/01answer/0106.txt", "../../../data/01answer/0107.txt", "../../../data/01answer/0108.txt", "../../../data/01answer/0109.txt", "../../../data/01answer/0110.txt" },
+	{ "../../../data/02answer/0101.txt", "../../../data/02answer/0102.txt", "../../../data/02answer/0103.txt", "../../../data/02answer/0104.txt", "../../../data/02answer/0105.txt", "../../../data/02answer/0106.txt", "../../../data/02answer/0107.txt", "../../../data/02answer/0108.txt", "../../../data/02answer/0109.txt", "../../../data/02answer/0110.txt" },
+	{ "../../../data/03answer/0101.txt", "../../../data/03answer/0102.txt", "../../../data/03answer/0103.txt", "../../../data/03answer/0104.txt", "../../../data/03answer/0105.txt", "../../../data/03answer/0106.txt", "../../../data/03answer/0107.txt", "../../../data/03answer/0108.txt", "../../../data/03answer/0109.txt", "../../../data/03answer/0110.txt" },
+	{ "../../../data/04answer/0101.txt", "../../../data/04answer/0102.txt", "../../../data/04answer/0103.txt", "../../../data/04answer/0104.txt", "../../../data/04answer/0105.txt", "../../../data/04answer/0106.txt", "../../../data/04answer/0107.txt", "../../../data/04answer/0108.txt", "../../../data/04answer/0109.txt", "../../../data/04answer/0110.txt" },
+	{ "../../../data/05answer/0101.txt", "../../../data/05answer/0102.txt", "../../../data/05answer/0103.txt", "../../../data/05answer/0104.txt", "../../../data/05answer/0105.txt", "../../../data/05answer/0106.txt", "../../../data/05answer/0107.txt", "../../../data/05answer/0108.txt", "../../../data/05answer/0109.txt", "../../../data/05answer/0110.txt" }
+};
+
+void  set_question(int a[][9], int *level, int *number);
+void  get_answer(int a[][9], int *level, int *number);
+
+void  set_question(int a[][9], int *level, int *number)
+{
+	FILE *fptr;
+	fptr = fopen(yunlin_road_question[*level - 1][*number - 1], "r");
+	if (fptr != NULL)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				fscanf(fptr, "%d", &a[i][j]);
+			}
+		}
+	}
+	else
+		printf("檔案開啟失敗");
+}
+
+void  get_answer(int a[][9], int *level, int *number)
+{
+	FILE *fptr;
+	fptr = fopen(yunlin_road_answer[*level - 1][*number - 1], "r");
+	if (fptr != NULL)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				fscanf(fptr, "%d", &a[i][j]);
+			}
+		}
+	}
+	else
+		printf("檔案開啟失敗");
+}
---
+++ yunlin/20170105/include/yunlin.h
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+char *yunlin_road_question[5][20] = 
+{
+	{ "../../../data/01question/0101.txt", "../../../data/01question/0102.txt", "../../../data/01question/0103.txt", "../../../data/01question/0104.txt", "../../../data/01question/0105.txt", "../../../data/01question/0106.txt", "../../../data/01question/0107.txt", "../../../data/01question/0108.txt", "../../../data/01question/0109.txt", "../../../data/01question/0110.txt", "../../../data/01question/0111.txt", "../../../data/01question/0112.txt", "../../../data/01question/0113.txt", "../../../data/01question/0114.txt", "../../../data/01question/0115.txt", "../../../data/01question/0116.txt", "../../../data/01question/0117.txt", "../../../data/01question/0118.txt", "../../../data/01question/0119.txt", "../../../data/01question/0120.txt" },
+	{ "../../../data/02question/0201.txt", "../../../data/02question/0202.txt", "../../../data/02question/0203.txt", "../../../data/02question/0204.txt", "../../../data/02question/0205.txt", "../../../data/02question/0206.txt", "../../../data/02question/0207.txt", "../../../data/02question/0208.txt", "../../../data/02question/0209.txt", "../../../data/02question/0210.txt", "../../../data/02question/0211.txt", "../../../data/02question/0212.txt", "../../../data/02question/0213.txt", "../../../data/02question/0214.txt", "../../../data/02question/0215.txt", "../../../data/02question/0216.txt", "../../../data/02question/0217.txt", "../../../data/02question/0218.txt", "../../../data/02question/0219.txt", "../../../data/02question/0220.txt" },
+	{ "../../../data/03question/0301.txt", "../../../data/03question/0302.txt", "../../../data/03question/0303.txt", "../../../data/03question/0304.txt", "../../../data/03question/0305.txt", "../../../data/03question/0306.txt", "../../../data/03question/0307.txt", "../../../data/03question/0308.txt", "../../../data/03question/0309.txt", "../../../data/03question/0310.txt", "../../../data/03question/0311.txt", "../../../data/03question/0312.txt", "../../../data/03question/0313.txt", "../../../data/03question/0314.txt", "../../../data/03question/0315.txt", "../../../data/03question/0316.txt", "../../../data/03question/0317.txt", "../../../data/03question/0318.txt", "../../../data/03question/0319.txt", "../../../data/03question/0320.txt" },
+	{ "../../../data/04question/0401.txt", "../../../data/04question/0402.txt", "../../../data/04question/0403.txt", "../../../data/04question/0404.txt", "../../../data/04question/0405.txt", "../../../data/04question/0406.txt", "../../../data/04question/0407.txt", "../../../data/04question/0408.txt", "../../../data/04question/0409.txt", "../../../data/04question/0410.txt", "../../../data/04question/0411.txt", "../../../data/04question/0412.txt", "../../../data/04question/0413.txt", "../../../data/04question/0414.txt", "../../../data/04question/0415.txt", "../../../data/04question/0416.txt", "../../../data/04question/0417.txt", "../../../data/04question/0418.txt", "../../../data/04question/0419.txt", "../../../data/04question/0420.txt" },
+	{ "../../../data/05question/0501.txt", "../../../data/05question/0502.txt", "../../../data/05question/0503.txt", "../../../data/05question/0504.txt", "../../../data/05question/0505.txt", "../../../data/05question/0506.txt", "../../../data/05question/0507.txt", "../../../data/05question/0508.txt", "../../../data/05question/0509.txt", "../../../data/05question/0510.txt", "../../../data/05question/0511.txt", "../../../data/05question/0512.txt", "../../../data/05question/0513.txt", "../../../data/05question/0514.txt", "../../../data/05question/0515.txt", "../../../data/05question/0516.txt", "../../../data/05question/0517.txt", "../../../data/05question/0518.txt", "../../../data/05question/0519.txt", "../../../data/05question/0520.txt" }
+};
+
+char *yunlin_road_answer[5][20] =
+{
+	{ "../../../data/01answer/0101.txt", "../../../data/01answer/0102.txt", "../../../data/01answer/0103.txt", "../../../data/01answer/0104.txt", "../../../data/01answer/0105.txt", "../../../data/01answer/0106.txt", "../../../data/01answer/0107.txt", "../../../data/01answer/0108.txt", "../../../data/01answer/0109.txt", "../../../data/01answer/0110.txt", "../../../data/01answer/0111.txt", "../../../data/01answer/0112.txt", "../../../data/01answer/0113.txt", "../../../data/01answer/0114.txt", "../../../data/01answer/0115.txt", "../../../data/01answer/0116.txt", "../../../data/01answer/0117.txt", "../../../data/01answer/0118.txt", "../../../data/01answer/0119.txt", "../../../data/01answer/0120.txt" },
+	{ "../../../data/02answer/0201.txt", "../../../data/02answer/0202.txt", "../../../data/02answer/0203.txt", "../../../data/02answer/0204.txt", "../../../data/02answer/0205.txt", "../../../data/02answer/0206.txt", "../../../data/02answer/0207.txt", "../../../data/02answer/0208.txt", "../../../data/02answer/0209.txt", "../../../data/02answer/0210.txt", "../../../data/02answer/0211.txt", "../../../data/02answer/0212.txt", "../../../data/02answer/0213.txt", "../../../data/02answer/0214.txt", "../../../data/01answer/0215.txt", "../../../data/02answer/0216.txt", "../../../data/02answer/0217.txt", "../../../data/02answer/0218.txt", "../../../data/02answer/0219.txt", "../../../data/02answer/0220.txt" },
+	{ "../../../data/03answer/0301.txt", "../../../data/03answer/0302.txt", "../../../data/03answer/0303.txt", "../../../data/03answer/0304.txt", "../../../data/03answer/0305.txt", "../../../data/03answer/0306.txt", "../../../data/03answer/0307.txt", "../../../data/03answer/0308.txt", "../../../data/03answer/0309.txt", "../../../data/03answer/0310.txt", "../../../data/03answer/0311.txt", "../../../data/03answer/0312.txt", "../../../data/03answer/0313.txt", "../../../data/03answer/0314.txt", "../../../data/01answer/0315.txt", "../../../data/03answer/0316.txt", "../../../data/03answer/0317.txt", "../../../data/03answer/0318.txt", "../../../data/03answer/0319.txt", "../../../data/03answer/0320.txt" },
+	{ "../../../data/04answer/0401.txt", "../../../data/04answer/0402.txt", "../../../data/04answer/0403.txt", "../../../data/04answer/0404.txt", "../../../data/04answer/0405.txt", "../../../data/04answer/0406.txt", "../../../data/04answer/0407.txt", "../../../data/04answer/0408.txt", "../../../data/04answer/0409.txt", "../../../data/04answer/0410.txt", "../../../data/04answer/0411.txt", "../../../data/04answer/0412.txt", "../../../data/04answer/0413.txt", "../../../data/04answer/0414.txt", "../../../data/01answer/0415.txt", "../../../data/04answer/0416.txt", "../../../data/04answer/0417.txt", "../../../data/04answer/0418.txt", "../../../data/04answer/0419.txt", "../../../data/04answer/0420.txt" },
+	{ "../../../data/05answer/0501.txt", "../../../data/05answer/0502.txt", "../../../data/05answer/0503.txt", "../../../data/05answer/0504.txt", "../../../data/05answer/0505.txt", "../../../data/05answer/0506.txt", "../../../data/05answer/0507.txt", "../../../data/05answer/0508.txt", "../../../data/05answer/0509.txt", "../../../data/05answer/0510.txt", "../../../data/05answer/0511.txt", "../../../data/05answer/0512.txt", "../../../data/05answer/0513.txt", "../../../data/05answer/0514.txt", "../../../data/01answer/0515.txt", "../../../data/05answer/0516.txt", "../../../data/05answer/0517.txt", "../../../data/05answer/0518.txt", "../../../data/05answer/0519.txt", "../../../data/05answer/0520.txt" }
+};
+
+void  set_question(int a[][9], int *level, int *number);
+void  get_answer(int a[][9], int *level, int *number);
+
+void  set_question(int a[][9], int *level, int *number)
+{
+	FILE *fptr;
+	fptr = fopen(yunlin_road_question[*level - 1][*number - 1], "r");
+	if (fptr != NULL)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				fscanf(fptr, "%d", &a[i][j]);
+			}
+		}
+	}
+	else
+		printf("檔案開啟失敗");
+}
+
+void  get_answer(int a[][9], int *level, int *number)
+{
+	FILE *fptr;
+	fptr = fopen(yunlin_road_answer[*level - 1][*number - 1], "r");
+	if (fptr != NULL)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				fscanf(fptr, "%d", &a[i][j]);
+			}
+		}
+	}
+	else
+		printf("檔案開啟失敗");
+}
